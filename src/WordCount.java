740811c76a1bc06ee8e4e1b6a6a7cc23333ca6ed
siyuanlyn <siyuanlyn@gmail.com>
2014-11-08 02:52:28 -0500

    map output emitted on the slave node collector added, map with dup key in File manager dataInput.readFully(data) throws EOFException - reaches the end before finish reading the input stream

--- src/WordCount.java
+++ src/WordCount.java
@@ -2,13 +2,17 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.StringTokenizer;
 
+import MapReduce.Library.Collector;
+import MapReduce.Library.Mapper;
+import MapReduce.Library.Pair;
+import MapReduce.Library.Reducer;
 
 public class WordCount {
 
-	public static class Map implements Mapper {
+	public static class Map implements Mapper<String, Integer> {
 
 		@Override
-		public void map(String key, String value, java.util.Map<String, Integer> collector) {
+		public void map(String key, String value, Collector<String, Integer> collector) {
 			String line = value.toString();
 			StringTokenizer tokenizer = new StringTokenizer(line);
 			while (tokenizer.hasMoreTokens()) {
@@ -17,9 +21,10 @@ public class WordCount {
 			}
 		}
 
+
 	}
 
-	public static class Reduce implements Reducer {
+	public static class Reduce implements Reducer<String, Integer> {
 
 		@Override
 		public void reduce(String key, List<Integer> values, java.util.Map<String, Integer> collector) {
@@ -35,5 +40,14 @@ public class WordCount {
 	
 	public static void main(String[] args){
 		// TODO
+		
+		Collector<String, Integer> c = new Collector<String, Integer>();
+		Map m = new Map();
+		m.map("", "a b c d a", c);
+		Iterator<Pair<String, Integer>> i = c.iterator();
+		while(i.hasNext()){
+			Pair<String, Integer> p = i.next();
+			System.out.println(p.getKey() + " | " + p.getValue());
+		}
 	}
 }
